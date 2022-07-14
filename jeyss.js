12990d4370a49c3f9085d03008a001b73099b6d3
Harrys <blackiaratanmusic@gmail.com>
2022-07-14 15:20:19 -0300

    project 85%

--- jeyss.js
+++ jeyss.js
@@ -1,28 +1,28 @@
 let toggles = document.getElementsByClassName
-('toggle');
+    ('toggle');
 let contentDiv = document.getElementsByClassName
-('conteudo');
+    ('conteudo');
 let icons = document.getElementsByClassName
-('icon');
+    ('icon');
 
-for(let i=0; i<toggles.length; i++){
-    toggles[i].addEventListener('click', ()=>{
-        if( parseInt(contentDiv[i].style.height)
-        != contentDiv[i].scrollHeight){
+for (let i = 0; i < toggles.length; i++) {
+    toggles[i].addEventListener('click', () => {
+        if (parseInt(contentDiv[i].style.height)
+            != contentDiv[i].scrollHeight) {
             contentDiv[i].style.height = contentDiv
             [i].scrollHeight + "px";
             toggles[i].style.color = "#111130";
             icons[i].classList.remove('fa-plus');
             icons[i].classList.add('fa-minus');
         }
-        else{
+        else {
             contentDiv[i].style.height = "0px";
             toggles[i].style.color = "#111130";
             icons[i].classList.remove('fa-minus');
             icons[i].classList.add('fa-plus');
         }
-        for(let j=o; j<contentDiv.length; j++){
-            if(j!==i){
+        for (let j = o; j < contentDiv.length; j++) {
+            if (j !== i) {
                 contentDiv[i].style.height = "0px";
                 toggles[i].style.color = "#111130";
                 icons[i].classList.remove('fa-minus');
@@ -30,4 +30,23 @@ for(let i=0; i<toggles.length; i++){
             }
         }
     })
+}
+
+function subScribe() {
+    var emailis = document.getElementById("emailis").value;
+
+    error_message.style.padding = "10px";
+
+    var text;
+    if (emailis == null || emailis == "") {
+        text = "Please Enter valid Name";
+        error_message.innerHTML = text;
+        document.getElementById('error_message').classList.remove('hide') = text;
+        return false;
+    }
+
+    alert("Form Submitted Successfully!");
+    document.getElementById('error_message').classList.add('hide');
+    var emailis = document.getElementById("emailis").value = "";
+    return true;
 }
