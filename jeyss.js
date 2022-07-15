458061aeca1663aa8e863ac28903e2f4c7176d73
Harrys <blackiaratanmusic@gmail.com>
2022-07-15 02:33:20 -0300

    project100%🎉

--- jeyss.js
+++ jeyss.js
@@ -50,3 +50,5 @@ function subScribe() {
     var emailis = document.getElementById("emailis").value = "";
     return true;
 }
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
