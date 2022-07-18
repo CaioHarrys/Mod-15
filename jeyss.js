514a3082d936937fc25bd94bf7d5d33b60c61649
Harrys <blackiaratanmusic@gmail.com>
2022-07-18 15:06:55 -0300

    🥳Atualização 100%🥳

--- jeyss.js
+++ jeyss.js
@@ -32,31 +32,28 @@ for (let i = 0; i < toggles.length; i++) {
     })
 }
 
-function subScribe() {
-    var emailis = document.getElementById("emailis").value;
-    var mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+function validatIon() {
+
+    var mail = document.getElementById("text").value;
+
+    var regx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
     error_message.style.padding = "10px";
 
-    var text;
-    if (emailis ==/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/||emailis==""||emailis==null) {
-        text = "Please Enter a Valid Email";
-        error_message.innerHTML = text;
-        document.getElementById('error_message').classList.remove('hide') = text;
-        return false;
-    }
+    var text = error_message;
 
-    alert("Form Submitted Successfully!");
-    document.getElementById('error_message').classList.add('hide');
-    var emailis = document.getElementById("emailis").value = "";
-    return true;
+    if (mail.match(regx)) {
+        alert("Form Submitted Successfully!");
+        document.getElementById('error_message').classList.add('hide');
+        var mail = document.getElementById("text").value = "";
+        return true;
+    }
+    text = "Invalid Email";
+    error_message.innerHTML = text;
+    document.getElementById('error_message').classList.remove('hide') = text;
+    return false;
+
 }
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
-
-function ValidateEmail(inputText) {
-    if (inputText.value.match(mailformat)) { document.form1.text1.focus(); alert("Valid email address!"); return true; }
-    else { alert("You have entered an invalid email address!"); document.form1.text1.focus(); return false; }
-}
